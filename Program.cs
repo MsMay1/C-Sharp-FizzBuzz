6dadfba0743fd090e9e860bba558b3f0d0cfc83a
Marion Maybury <Marion.Maybury@softwire.com>
2021-09-20 11:23:28 +0100

    Print 1 - 100

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace C_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i=1; i<=100; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
