5c2ea87785b981e390078b59c28488bcfe548a5a
Marion Maybury <Marion.Maybury@softwire.com>
2021-09-20 16:13:51 +0100

    Change to individual methods

--- Program.cs
+++ Program.cs
@@ -4,24 +4,70 @@ namespace C_
 {
     class Program
     {
+        static string FizzRule(int num, string print)
+        {
+            if(num%3 == 0){
+                print += "Fizz";
+            }
+
+            return print;
+        }
+
+        static string FezzRule(int num, string print)
+        {
+            if(num%13 == 0){
+                print += "Fezz";
+            }
+
+            return print;
+        }
+
+        static string BuzzRule(int num, string print)
+        {
+            if(num%5 == 0){
+                print += "Buzz";
+            }
+
+            return print;
+        }
+
+        static string BangRule(int num, string print)
+        {
+            if(num%7 == 0){
+                print += "Bang";
+            }
+
+            return print;
+        }
+        static string BongRule(int num, string print)
+        {
+            if(num%11 == 0 && num%13 == 0){
+                print += "Bong";
+            } else if (num % 11 == 0){
+                print = "Bong";
+            }
+
+            return print;
+        }
+
         static void Main(string[] args)
         {
-            for (int i=1; i<=100; i++)
+            for (int i=1; i<=145; i++)
             {
-                if(i%3 == 0 && i%5 == 0){
-                    Console.WriteLine("FizzBuzz");
-                }
-                else if(i%3 == 0){
-                    Console.WriteLine("Fizz");
-                }
-                else if(i%5 == 0){
-                    Console.WriteLine("Buzz");
-                }
-                else {
-                    Console.WriteLine(i);  
-                }
+                string print = "";
+                print = FizzRule(i, print);
+                print = FezzRule(i, print);
+                print = BuzzRule(i, print);
+                print = BangRule(i, print);
+                print = BongRule(i, print);
+                Console.WriteLine(print == "" ? i : print);
+
                 
             }
         }
     }
 }
+
+// create list 
+// each method add to list
+// console.log list
